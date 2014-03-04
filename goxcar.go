404d54775ed7402eed63ff463f3eba1f17df5b71
Javier Segura <javism@gmail.com>
2014-03-04 14:04:30 +0100

    Initial import

---
+++ goxcar.go
@@ -0,0 +1,28 @@
+package goxcar
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+const (
+	ENDPOINT = "https://new.boxcar.io/api/notifications"
+)
+
+func notify(credentials string, title string, long_message string, sound string) (err error) {
+
+	values := make(url.Values)
+	values.Set("user_credentials", credentials)
+	values.Set("notification[title]", title)
+	values.Set("notification[long_message]", long_message)
+	values.Set("notification[sound]", sound)
+
+	r, err := http.PostForm(ENDPOINT, values)
+	if err != nil {
+		return err
+	}
+	body, _ := ioutil.ReadAll(r.Body)
+	r.Body.Close()
+	fmt.Printf("body: %s", body)
+}
